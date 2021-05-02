80a71e85c8c0ba1d1ee17ef8e8a4485cafadf252
predragstosic1985 <predragstosic1985@gmail.com>
2021-05-02 18:38:08 +0200

    Initial backend-fronted comunication setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [fireBaseTest, setFireBaseTest] = useState(null);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+  useEffect(() => {
+    const initialfetch = async () => {
+      const tt = await fetch("/api/hello/read").then((res) => res.json());
+
+      // await fetch("/api/hello/read").then((res) => setFireBaseTest(res.id));
+      console.log(tt);
+      setFireBaseTest(tt[0].id);
+      return tt;
+    };
+    initialfetch();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{!data ? "Loading..." : data}</p>
+        <p>{!fireBaseTest ? "Loading from firebase..." : fireBaseTest}</p>
       </header>
     </div>
   );
